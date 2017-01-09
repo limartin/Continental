77d2b7a2f3bae9b212c5fc0ac066c40c08fec0a8
Luis Martinez <limartin71@outlook.com>
2017-01-08 22:11:39 -0800

    Add initial unittests

---
+++ Continental.Tests/DeckCardsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Continental;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continental.Tests
+{
+    [TestClass()]
+    public class DeckCardsTests
+    {
+        [TestMethod()]
+        public void DeckCardsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
